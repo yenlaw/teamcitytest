703337901d98ca4ae2394f35a71883249eb034c8
yenlaw <yenlaw@zoho.com>
2017-01-09 15:14:44 +0000

    test

---
+++ cmake-build-debug/CMakeFiles/teamcitytest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teamcitytest.dir/main.cpp.o"
+  "teamcitytest.pdb"
+  "teamcitytest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teamcitytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
